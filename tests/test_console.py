ca2569f429e2938b60bda73551b860a3a368dba1
Charles DZADU <dkccharlie@gmail.com>
2023-10-15 23:10:44 +0000

    feat: Add basic test

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,33 +1,66 @@
 #!/usr/bin/python3
-"""Unittest for console.py
-
 """
-import unittest
-from ..console import HBNBCommand
+Unit tests for console using Mock module from python standard library
+Checks console for capturing stdout into a StringIO object
+"""
+
 import os
+import sys
+import unittest
+from unittest.mock import create_autospec, patch
+from io import StringIO
+from console import HBNBCommand
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
-class TestAirbnbClone_prompting(unittest.TestCase):
+class TestConsole(unittest.TestCase):
     """
-    Unittests for testing the console.py file
+    Unittest for the console model
     """
 
-    @classmethod
-    def setUpClass(cls):
-        """Set up class method for the test cases"""
-        cls.console = HBNBCommand()
+    def setUp(self):
+        """Redirecting stdin and stdout"""
+        self.mock_stdin = create_autospec(sys.stdin)
+        self.mock_stdout = create_autospec(sys.stdout)
+        self.err = ["** class name missing **",
+                    "** class doesn't exist **",
+                    "** instance id missing **",
+                    "** no instance found **",
+                    ]
 
-    @classmethod
-    def tearDownClass(cls):
+        self.cls = ["BaseModel",
+                    "User",
+                    "State",
+                    "City",
+                    "Place",
+                    "Amenity",
+                    "Review"]
+
+    def create(self, server=None):
         """
-        Tear down class method for the test cases
-        Delete the file.json created
+        Redirects stdin and stdout to the mock module
         """
-        try:
-            os.remove("file.json")
-        except FileNotFoundError:
-            pass
+        return HBNBCommand(stdin=self.mock_stdin, stdout=self.mock_stdout)
+
+    def last_write(self, nr=None):
+        """Returns last n output lines"""
+        if nr is None:
+            return self.mock_stdout.write.call_args[0][0]
+        return "".join(map(lambda c: c[0][0],
+                           self.mock_stdout.write.call_args_list[-nr:]))
+
+    def test_quit(self):
+        """Quit command"""
+        cli = self.create()
+        self.assertTrue(cli.onecmd("quit"))
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Test suits for File storage
+"""
+import os
+import models
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestFileStorage(unittest.TestCase):
+    """
+    Tests for file storage
+    """
+
+    def test_name(self):
+        """
+        Tests for name for file storage
+        """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
